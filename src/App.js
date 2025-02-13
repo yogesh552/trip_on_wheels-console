8889cf6ff55dac2133163fd67d8e481f7701952e
Yogesh <yogeshmanthanitsolution@gmail.com>
2025-02-13 15:06:24 +0530

    folder created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+const layout =()=>{
+  return(
+    <>
+      <div>
+        <h1>Header</h1>
+        <h1>Content</h1>
+        <h1>Footer</h1>
+      </div>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   layout() 
   );
 }
+
+
 
 export default App;
